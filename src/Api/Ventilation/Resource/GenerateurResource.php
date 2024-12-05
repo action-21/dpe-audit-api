915365e558236795f5e3c5981e0690229982c6e3
Adrien Rosi <adrienrosi@gmail.com>
2024-12-05 17:58:42 +0100

    refacto ventilation

--- src/Api/Ventilation/Resource/GenerateurResource.php
+++ src/Api/Ventilation/Resource/GenerateurResource.php
@@ -3,7 +3,7 @@
 namespace App\Api\Ventilation\Resource;
 
 use App\Domain\Common\Type\Id;
-use App\Domain\Ventilation\Enum\{TypeGenerateur, TypeVentilation};
+use App\Domain\Ventilation\Enum\{TypeGenerateur, TypeVmc};
 use App\Domain\Ventilation\Entity\{Generateur as Entity, GenerateurCollection as EntityCollection};
 
 final class GenerateurResource
@@ -11,8 +11,8 @@ final class GenerateurResource
     public function __construct(
         public readonly Id $id,
         public readonly string $description,
-        public readonly TypeVentilation $type_ventilation,
         public readonly TypeGenerateur $type,
+        public readonly ?TypeVmc $type_vmc,
         public readonly bool $presence_echangeur_thermique,
         public readonly bool $generateur_collectif,
         public readonly ?int $annee_installation,
@@ -23,8 +23,8 @@ final class GenerateurResource
         return new self(
             id: $entity->id(),
             description: $entity->description(),
-            type_ventilation: $entity->type_ventilation(),
             type: $entity->type(),
+            type_vmc: $entity->type_vmc(),
             presence_echangeur_thermique: $entity->presence_echangeur_thermique(),
             generateur_collectif: $entity->generateur_collectif(),
             annee_installation: $entity->annee_installation(),
--- src/Api/Ventilation/Resource/SystemeResource.php
+++ src/Api/Ventilation/Resource/SystemeResource.php
@@ -5,7 +5,7 @@ namespace App\Api\Ventilation\Resource;
 use App\Domain\Common\Type\Id;
 use App\Domain\Common\ValueObject\Consommation;
 use App\Domain\Ventilation\Entity\{Systeme as Entity, SystemeCollection as EntityCollection};
-use App\Domain\Ventilation\Enum\{ModeExtraction, ModeInsufflation, TypeSysteme};
+use App\Domain\Ventilation\Enum\TypeVentilation;
 use App\Domain\Ventilation\ValueObject\Performance;
 
 /**
@@ -16,9 +16,7 @@ final class SystemeResource
     public function __construct(
         public readonly Id $id,
         public readonly ?Id $generateur_id,
-        public readonly TypeSysteme $type,
-        public readonly ?ModeExtraction $mode_extraction,
-        public readonly ?ModeInsufflation $mode_insufflation,
+        public readonly TypeVentilation $type_ventilation,
         public readonly ?float $rdim,
         public readonly ?Performance $performance,
         /** @var Consommation[] */
@@ -30,9 +28,7 @@ final class SystemeResource
         return new self(
             id: $entity->id(),
             generateur_id: $entity->generateur()?->id(),
-            type: $entity->type(),
-            mode_extraction: $entity->mode_extraction(),
-            mode_insufflation: $entity->mode_insufflation(),
+            type_ventilation: $entity->type_ventilation(),
             rdim: $entity->rdim(),
             performance: $entity->performance(),
             consommations: $entity->consommations()?->values() ?? [],
